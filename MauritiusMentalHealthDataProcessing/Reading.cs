bd28748062e62d0133596201c208fe345784a312
Raxxit <raxxit004@gmail.com>
2026-01-05 16:51:07 +0400

    Added Xsd File and read method

---
+++ MauritiusMentalHealthDataProcessing/Reading.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+namespace MauritiusMentalHealthDataProcessing
+{
+    public class Reading
+    {
+        public XDocument ReadXmlFile()
+        {
+            XDocument doc = XDocument.Load("/xml/acme_mental_health_programs.xml");
+            return doc;
+        }
+
+        
+
+
+
+    }
+}
