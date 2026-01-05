7dc77a6a329419616a2dc3cc4d494320dadeed30
Raxxit <raxxit004@gmail.com>
2026-01-05 18:02:47 +0400

    Completed Reading and validation method

--- MauritiusMentalHealthDataProcessing/Program.cs
+++ MauritiusMentalHealthDataProcessing/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+
+namespace MauritiusMentalHealthDataProcessing
+{
+    internal class Program 
+    { 
+
+        static void Main(string[] args)
+        {
+            Reading reading = new Reading();
+            XDocument doc = reading.ReadXmlFile();
+            bool isValid = reading.ValidateXml(doc);
+        }
+
+
+    }
+}
--- MauritiusMentalHealthDataProcessing/Reading.cs
+++ MauritiusMentalHealthDataProcessing/Reading.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.Linq;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
 namespace MauritiusMentalHealthDataProcessing
 {
@@ -6,11 +9,39 @@ namespace MauritiusMentalHealthDataProcessing
     {
         public XDocument ReadXmlFile()
         {
-            XDocument doc = XDocument.Load("/xml/acme_mental_health_programs.xml");
+            XDocument doc = XDocument.Load("xml/acme_mental_health_programs.xml");
             return doc;
         }
 
-        
+        public bool ValidateXml(XDocument doc)
+        {
+
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", "xml/acmeSchema.xsd");
+
+            bool isValid = true;
+            string errorMessage = "";
+
+            doc.Validate(schemas, (o, e) =>
+            {
+                isValid = false;
+                errorMessage += e.Message + "\n";
+            });
+
+            if (!isValid)
+            {
+                Console.WriteLine("XML Validation Errors:\n" + errorMessage);
+            }
+            else
+            {
+                Console.WriteLine("XML is valid against the schema.");
+
+            }
+            
+            return isValid;
+
+        }
+
 
 
 
