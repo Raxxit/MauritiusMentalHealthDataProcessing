64efbafa7d91b6794acaec3f5077d07431cb4aaa
zendrax26 <jugnarain1234@gmail.com>
2026-01-06 14:39:10 +0400

    soriting done

---
+++ MauritiusMentalHealthDataProcessing/DriverRisk.cs
@@ -0,0 +1,9 @@
+﻿namespace MauritiusMentalHealthDataProcessing
+{
+    internal class DriverRisk
+    {
+        public string DriverID { get; set; }
+        public string RiskLevel { get; set; }
+        public int NumberOfIncidents { get; set; }
+    }
+}
